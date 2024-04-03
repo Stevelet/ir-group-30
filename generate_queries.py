37728027a4c5fb5c23011f369a1f083e6603c7b3
tijn28 <stijn.swart@gmail.com>
2024-04-04 01:05:36 +0200

    Changes

--- generate_queries.py
+++ generate_queries.py
@@ -1,18 +1,46 @@
-from transformers import T5Tokenizer, DataCollatorForSeq2Seq
-from transformers import T5ForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
+# import torch
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+#
+# model_name = "huggyllama/llama-7b"
+#
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_name,
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_compute_dtype=torch.bfloat16,
+#     device_map="cpu"
+# )
+# tokenizer = AutoTokenizer.from_pretrained(
+#     model_name,
+#     trust_remote_code=True,
+#     device_map="cpu"
+# )
+#
+# batch = tokenizer(
+#     "The capital of Canada is",
+#     return_tensors="pt",
+#     add_special_tokens=False
+# )
+#
+# print("Reached")
+#
+# batch = {k: v.to("cpu") for k, v in batch.items()}
+# generated = model.generate(batch["input_ids"], max_length=100)
+# print(tokenizer.decode(generated[0]))
 
-MODEL_NAME = "google/flan-t5-xl"
+import transformers
+from dotenv import load_dotenv
 
-tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
-model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
-data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+load_dotenv()
+
+device = "cpu"
+
+tokenizer = transformers.LlamaTokenizer.from_pretrained("huggyllama/llama-7b")
+model = transformers.LlamaForCausalLM.from_pretrained("huggyllama/llama-7b", load_in_4bit=True)
 
 batch = tokenizer(
     "The capital of Canada is",
     return_tensors="pt",
     add_special_tokens=False
 )
-
-batch = {k: v.to("cpu") for k, v in batch.items()}
-generated = model.generate(batch["input_ids"], max_length=100)
-print(tokenizer.decode(generated[0]))
