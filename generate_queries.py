f9acb471e47cda8ee2d0a331f17f11613e990b9d
stijn <stijn.swart@gmail.com>
2024-04-04 11:55:13 +0200

    Download index

--- generate_queries.py
+++ generate_queries.py
@@ -1,5 +1,6 @@
 import transformers
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
@@ -11,6 +12,7 @@ k_drive = os.path.join(os.getcwd(), "..", "..", "..", "mnt", "k")
 cache_path = os.path.join(k_drive, "cache")
 trec_data = os.path.join(k_drive, "trec")
 terrier_path = os.path.join(k_drive, "terrier")
+index_path = os.path.join(k_drive, "index")
 
 
 # tokenizer = transformers.LlamaTokenizer.from_pretrained("huggyllama/llama-7b", cache_dir=cache_path)
@@ -23,11 +25,11 @@ if not pt.started():
     pt.init(tqdm="notebook", home_dir=terrier_path)
 
 dataset = pt.get_dataset('irds:trec-fair/2022')
-
-iter = dataset.get_corpus_iter()
-
-for item in iter:
-    pass
+idx_path = Path(index_path).absolute()
+if not (idx_path / "data.properties").is_file():
+    pt.index.IterDictIndexer(
+        str(idx_path)
+    ).index(dataset.get_corpus_iter())
 
 # batch = tokenizer(
 #     "The capital of Canada is",
